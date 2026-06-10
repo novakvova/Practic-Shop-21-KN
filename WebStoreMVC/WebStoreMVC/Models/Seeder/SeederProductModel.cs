6b8d4d67d162a71a1c0879bcb591f685a66ca6e7
novakvova <novakvova@gmail.com>
2026-06-10 11:47:17 +0300

    Add Seed Data Begin Product

---
+++ WebStoreMVC/WebStoreMVC/Models/Seeder/SeederProductModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebStoreMVC.Models.Seeder;
+
+public class SeederProductModel
+{
+    public string Name { get; set; } = String.Empty;
+    public string Slug { get; set; } = String.Empty;
+    public double Price { get; set; }
+    public string Description { get; set; } = String.Empty;
+    public List<string> Images { get; set; } = null!;
+}
